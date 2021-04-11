31eb4b467d95eec8dcff2cb6dd02809cb45f0a50
yoji7 <yo.z940127@gmail.com>
2021-04-11 16:01:50 +0000

    modify index

---
+++ components/TextImageCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card-container text-center pr-10 mx-auto">
+    <v-img :src="`${src}`"></v-img>
+    <v-card :color="`${backgroundColor}`">
+      <div
+        :class="`card-text d-flex align-end pa-3 text-h5 text-left font-weight-medium ${color}--text`"
+      >{{text}}</div>
+    </v-card>
+  </div>
+</template>
+
+<style scoped>
+.card-container {
+  height: 100%;
+  max-width: 300px;
+  position: relative;
+}
+
+.v-image {
+  height: 100%;
+  border-radius: 10% !important;
+}
+
+.v-card {
+  height: calc(50% + 20px);
+  width: calc(50% - 25px);
+  position: absolute;
+  top: 50%;
+  left: calc(50% + 10px);
+  border-radius: 10% !important;
+}
+
+.v-card .card-text {
+  height: 100%;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    text: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    color: {
+      type: String,
+      default: 'white',
+      required: false,
+    },
+    backgroundColor: {
+      type: String,
+      default: 'primary',
+      required: false,
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,106 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetif + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container>
+    <v-row>
+      <v-col cols=12 md=6>
+        <v-row class="mb-3">
+          <v-col cols=12>
+            <div class="text-h5 font-weight-medium">
+              Lassana Delights Your Daily Coffee Experience
+            </div>
+          </v-col>
+          <v-col cols=12>
+            <div class="text-body2">
+              Lassana is a Hong Kong local brand that specialize in coffee and fresh bakery. We are here to provide excellent food service that inspire you everyday.
+            </div>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols=12 md=6>
+        <v-img src="/v.png" contain=true  max-height="250px"></v-img>
+      </v-col>
+    </v-row>
+    <v-row class="my-3">
+      <v-col cols=12>
+        <v-carousel cycle :show-arrows="false" :hide-delimiter-background="true" style="max-height: 300px;">
+          <v-carousel-item
+            v-for="(item,i) in carouselItems"
+            :key="i"
+            :src="item.src"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+    <v-row class="my-3">
+      <v-col
+        class="my-3"
+        cols=12
+        md=4
+        v-for="(card,i) in textImageCards"
+        :key="i"
+      >
+        <TextImageCard v-bind="card" />
+      </v-col>
+    </v-row>
+    <v-row  class="mt-3">
+      <v-col cols=12>
+        <v-row>
+          <v-col cols=12>
+            <div class="text-subtitle1 font-weight-medium">
+              For Further Information About Lassana Service including Coffee and Fresh Bakery
+            </div>
+          </v-col>
+        </v-row>
+        <v-row align="center" justify="start">
+          <v-col cols=12>
+            <v-btn :to="`${contactButton.to}`" large depressed class="primary text-h6" nuxt>{{contactButton.text}}</v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import TextImageCard from '~/components/TextImageCard.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    TextImageCard
+  },
+  data() {
+    return {
+      carouselItems: [
+        {
+          src: '/icon.png'
+        },
+        {
+          src: '/vuetify-logo.svg'
+        },
+        {
+          src: 'https://jp.vuejs.org/images/logo.png'
+        }
+      ],
+      textImageCards: [
+        {
+          src: 'https://webnexty.com/wp-content/uploads/2015/03/ffffff.png',
+          text: 'Our Coffee',
+          backgroundColor: 'black'
+        },
+        {
+          src: 'https://webnexty.com/wp-content/uploads/2015/03/ffffff.png',
+          text: 'Our Pastry'
+        },
+        {
+          src: 'https://webnexty.com/wp-content/uploads/2015/03/ffffff.png',
+          text: 'Our Food',
+          backgroundColor: 'light-green darken-4'
+        }
+      ],
+      contactButton: {
+        to: '/contact',
+        text: 'Contact Us'
+      }
+    }
   }
 }
 </script>
